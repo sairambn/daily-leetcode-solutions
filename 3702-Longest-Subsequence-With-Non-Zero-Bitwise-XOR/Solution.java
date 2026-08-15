0afeb9806da0c1bcdf5533f9f17b33230a693ab5
Sairam BN <bnsairam14@gmail.com>
2026-08-15 05:49:47 +0530

    Add solution for 3702. Longest Subsequence With Non-Zero Bitwise XOR

---
+++ 3702-Longest-Subsequence-With-Non-Zero-Bitwise-XOR/Solution.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int longestSubsequence(int[] nums) {
+        int n = nums.length;
+        int xor = 0;
+        boolean hasNonZero = false;
+
+        for (int num : nums) {
+            xor ^= num;
+            if (num != 0) {
+                hasNonZero = true;
+            }
+        }
+
+        if (xor != 0) {
+            return n;
+        }
+        return hasNonZero ? n - 1 : 0;
+    }
+}
